7c7d1e92e9dc06594d6d840869d171a70a75965b
Evan Hesketh <evanhesketh@yahoo.ca>
2023-03-07 18:51:21 -0500

    add part 2 extract_words

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,18 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        """ takes a path to a text file, extracts list of words, prints
+            message: [num-of-words-read] words read """
+
+        self.path = path
+        self.list_of_words = []
+        # self.print_message
+
+    def extract_words(self) :
+        """ read file and extract words to list_of_words """
+
+        file = open(self.path)
+
+        for line in file:
+            self.list_of_words.append(line[:-1])
