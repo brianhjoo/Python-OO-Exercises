cd6e71f23b9accd1ed75ecb4ef38c835fa017cdc
Brian Joo <brianhjoo@gmail.com>
2023-03-07 16:10:15 -0800

    added printNumOfWordsRead and random class methods

--- wordfinder.py
+++ wordfinder.py
@@ -1,3 +1,6 @@
+from random import choice
+
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
@@ -6,13 +9,29 @@ class WordFinder:
             message: [num-of-words-read] words read """
 
         self.path = path
-        self.list_of_words = []
-        # self.print_message
+        self.list_of_words = self.extract_words()
+        self.printNumOfWordsRead()
 
     def extract_words(self) :
         """ read file and extract words to list_of_words """
 
+        list_of_words = []
+
         file = open(self.path)
 
         for line in file:
-            self.list_of_words.append(line[:-1])
+            list_of_words.append(line[:-1])
+
+        return list_of_words
+
+    def printNumOfWordsRead(self):
+        """ gets the number of extracted words from the file in path and
+        prints that number out when making a new class """
+
+        print(f'{len(self.list_of_words)} words read')
+
+    def random(self):
+        """ returns a random word from list_of_words """
+
+        return choice(self.list_of_words)
+
